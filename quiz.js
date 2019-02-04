d3898e4756e293021b5fc127edcb914b1fbdd2a2
Loki <zpk.loki@gmail.com>
2019-02-04 19:12:57 +0300

    new_file

---
+++ quiz.js
@@ -0,0 +1,36 @@
+const smallestDivisor = (num) => {
+
+    const iter = (counter, num) => {
+        if (num % counter === 0 && counter > 1) {
+            return counter;
+        }
+        else if (num === counter) {
+            return counter;
+        }
+
+        return iter(counter + 1, num);
+
+    };
+
+    return iter(1, num);
+
+};
+console.log (smallestDivisor(16));
+
+const sequenceSum = (begin, end) => {
+
+    if (begin > end) {
+        return NaN;
+    }
+
+    else if (begin === end) {
+        return begin;
+
+    }
+    else {
+        return begin + sequenceSum(begin + 1, end);
+    }
+
+};
+
+console.log (sequenceSum(2, 6));
