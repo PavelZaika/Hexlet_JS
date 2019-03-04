4716a14c68049a30dc35cfc740e5c56bd989f9fc
Loki <zpk.loki@gmail.com>
2019-03-04 17:19:58 +0300

    new quiz

--- areBracketsBalanced.js
+++ areBracketsBalanced.js
@@ -8,6 +8,20 @@
     Закрывающая скобка не должна идти впереди открывающей.
  */
 
+ /** 
+  export default (str) => {
+  let acc = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    const symbol = str[i];
+    acc = symbol === '(' ? acc + 1 : acc - 1;
+    if (acc < 0) {
+      return false;
+    }
+  }
+  return acc === 0;
+};
+ */
+
 
  const areBracketsBalanced = (str) => {
 let left='';
